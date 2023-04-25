e3597f76a744f935f2723a6990cb115cb3516b60
soubj <sobujahmed124@gmail.com>
2023-04-25 10:40:36 +0600

    basic compos and added outlet

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <h2 className="text-4xl font-bold">this is header.</h2>
-    </div>
+    <nav className=" w-2/4 mx-auto text-white font-bold text-center border- mt-4 bg-cyan-400 p-4">
+      <Link to="/">Home</Link>
+      <Link to="/meals" className="ml-6">
+        Meals
+      </Link>
+    </nav>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,18 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Home/Home";
+import Meals from "./components/Meals/Meals";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home></Home>,
+    children: [
+      {
+        path: "meals",
+        element: <Meals></Meals>,
+      },
+    ],
   },
 ]);
 
